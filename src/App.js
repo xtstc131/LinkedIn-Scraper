f9487dee05ec3d603f9356a755531289593e124e
malloxia <mallxia@163.com>
2020-07-23 21:35:54 -0400

    Fix some code to deploy on Netlify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 // import './App.css';
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import MultiSortTable from "./components/table";
 import {
   Container,
-  Row,
   Col,
   Navbar,
   Nav,
--- src/components/table.js
+++ src/components/table.js
@@ -24,6 +24,7 @@ const defaultSorted = [
   }
 ];
 
+// eslint-disable-next-line react/display-name
 export default () => (
   <div>
     <ToolkitProvider
