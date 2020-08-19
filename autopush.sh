8d586634bea54dcf1a59574ecfedf833183ef97a
malloxia <mallxia@163.com>
2020-08-19 00:30:11 -0400

    Some updates

--- autopush.sh
+++ autopush.sh
@@ -5,7 +5,7 @@ DATE_N=`/bin/date "+%Y-%m-%d %H:%M:%S"`
 /usr/local/bin/git add .
 /usr/local/bin/git commit -m "${DATE_N} Update"
 
-/usr/local/bin/git push
+/usr/local/bin/git push origin master
 
 echo "Done"
 exit 0
